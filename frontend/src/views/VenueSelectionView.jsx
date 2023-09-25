d82417df3d1960514f374a0327c23d4fa78ca923
valera <victorbobkov@gmail.com>
2023-09-25 04:36:53 +0300

    Move assets into public folder

--- frontend/src/views/VenueSelectionView.jsx
+++ frontend/src/views/VenueSelectionView.jsx
@@ -5,17 +5,17 @@ import '../styles/VenueSelectionView.css';
 
 const VenueSelectionView = () => {
   const venueTypes = [
-    { type: 'Apartment', iconPath: 'src/assets/lotties/House.json' },
-    { type: 'Resort', iconPath: 'src/assets/lotties/Resort.json' },
-    { type: 'Camping', iconPath: 'src/assets/lotties/Camping.json' },
-    { type: 'Amusement', iconPath: 'src/assets/lotties/RollerCoaster.json' },
+    { type: 'Apartment', iconPath: '/assets/lotties/House.json' },
+    { type: 'Resort', iconPath: '/assets/lotties/Resort.json' },
+    { type: 'Camping', iconPath: '/assets/lotties/Camping.json' },
+    { type: 'Amusement', iconPath: '/assets/lotties/RollerCoaster.json' },
   ];
 
   const venues = [
-    { id: 1, name: 'Name 1', rating: 4.5, price: '$150', imageUrl: 'src/assets/images/art1.jpg' },
-    { id: 2, name: 'Name 2', rating: 4.5, price: '$150', imageUrl: 'src/assets/images/art2.jpg' },
-    { id: 3, name: 'Name 3', rating: 4.5, price: '$150', imageUrl: 'src/assets/images/art3.jpg' },
-    { id: 4, name: 'Name 4', rating: 4.5, price: '$150', imageUrl: 'src/assets/images/art4.jpg' },
+    { id: 1, name: 'Name 1', rating: 4.5, price: '$150', imageUrl: '/assets/images/art1.jpg' },
+    { id: 2, name: 'Name 2', rating: 4.5, price: '$150', imageUrl: '/assets/images/art2.jpg' },
+    { id: 3, name: 'Name 3', rating: 4.5, price: '$150', imageUrl: '/assets/images/art3.jpg' },
+    { id: 4, name: 'Name 4', rating: 4.5, price: '$150', imageUrl: '/assets/images/art4.jpg' },
   ];
 
   const handleTypeClick = (type) => {
