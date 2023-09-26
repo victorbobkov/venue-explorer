6bb673a8676974f03d01fd755a261ea32ec883b2
valera <victorbobkov@gmail.com>
2023-09-26 05:11:24 +0300

    Add sorting by venue types

--- frontend/src/views/VenueSelectionView.jsx
+++ frontend/src/views/VenueSelectionView.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import VenueType from '../components/VenueType.jsx';
 import VenueList from '../components/VenueList.jsx';
 import '../styles/VenueSelectionView.css';
@@ -12,16 +13,21 @@ const VenueSelectionView = () => {
   ];
 
   const venues = [
-    { id: 1, name: 'Urbanite High-Rise', type: 'Apartment', rating: 4.5, price: '$130', imageUrl: '/assets/images/art1.jpg' },
-    { id: 2, name: 'Sapphire Shores', type: 'Resort', rating: 4.5, price: '$325', imageUrl: '/assets/images/art2.jpg' },
-    { id: 3, name: 'Crestwood Grounds', type: 'Camping', rating: 4.5, price: '$45', imageUrl: '/assets/images/art3.jpg' },
-    { id: 4, name: 'Whimsical Wonderland', type: 'Amusement', rating: 4.5, price: '$80', imageUrl: '/assets/images/art4.jpg' },
+    { id: 1, name: 'Urbanite High-Rise', type: 'Apartment', rating: 4.7, price: '$130', imageUrl: '/assets/images/art1.jpg', description: '' },
+    { id: 2, name: 'Sapphire Shores', type: 'Resort', rating: 5.0, price: '$325', imageUrl: '/assets/images/art2.jpg', description: '' },
+    { id: 3, name: 'Crestwood Grounds', type: 'Camping', rating: 4.4, price: '$45', imageUrl: '/assets/images/art3.jpg', description: '' },
+    { id: 4, name: 'Whimsical Wonderland', type: 'Amusement', rating: 4.5, price: '$80', imageUrl: '/assets/images/art4.jpg', description: '' },
   ];
 
-  // Handles venue type selections
+  const [selectedType, setSelectedType] = useState('');
+
   const handleTypeClick = (type) => {
-    console.log('Venue Type Clicked:', type);
+    setSelectedType(type);
   };
+
+  const displayedVenues = selectedType
+    ? venues.filter((venue) => venue.type === selectedType)
+    : venues;
 
   return (
     <section className="venue-selection">
@@ -30,7 +36,7 @@ const VenueSelectionView = () => {
           <VenueType key={type.type} {...type} onTypeClick={() => handleTypeClick(type.type)} />
         ))}
       </div>
-      <VenueList venues={venues} />
+      <VenueList venues={displayedVenues} />
     </section>
   );
 };
