465f9825c42712643a172823982d00f53735d8e2
valera <victorbobkov@gmail.com>
2023-10-03 23:31:18 +0300

    Initialize bot launch

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const express = require('express');
 const venueRoutes = require('./src/routes/venueRoutes');
 const devCors = require('./src/middlewares/devCors');
 
+const { startBot } = require('./src/bot');
+startBot();
+
 const app = express();
 app.use(devCors);
 app.use('/api', venueRoutes);
 
 const PORT = 80;
-app.listen(PORT, () => console.log(`Bot is up and running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/src/bot.js
+++ server/src/bot.js
@@ -4,15 +4,22 @@ const webAppUrl = 'https://venue-explorer.vercel.app/';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on('message', async (msg) => {
-  const chatId = msg.chat.id;
+const startBot = () => {
+  bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
 
-  await bot.sendMessage(chatId, 'Welcome to the Venue Booking Bot! \n\nPlease tap the button below to get started.', {
-    reply_markup: {
-      inline_keyboard: [
-        [{text: 'Start Booking', web_app: { url: webAppUrl }}]
-      ],
-      resize_keyboard: true,
-    }
-  })
-});
+    await bot.sendMessage(chatId, 'Welcome to the Venue Booking Bot! \n\nPlease tap the button below to get started.', {
+      reply_markup: {
+        inline_keyboard: [
+          [{text: 'Start Booking', web_app: { url: webAppUrl }}]
+        ],
+        resize_keyboard: true,
+      }
+    })
+  });
+
+  console.log('Bot is running...')
+};
+
+module.exports = { startBot };
+
