c82ec430ad85ec214cbf6a4dd15738e06459bdca
valera <victorbobkov@gmail.com>
2023-10-07 03:48:25 +0300

    Add bot handling of pre-checkout query

--- server/src/bot.js
+++ server/src/bot.js
@@ -5,44 +5,35 @@ const webAppUrl = 'https://venue-explorer.vercel.app/';
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 const startBot = () => {
-  bot.on('message', async (msg) => {
+  bot.on('message',  async (msg) => {
     const chatId = msg.chat.id;
 
     await bot.sendMessage(chatId, 'Adventure Awaits! 🌍\n\nTap below and let\'s discover your next charming venue together!', {
       reply_markup: {
         inline_keyboard: [
           [{text: 'Start Booking', web_app: { url: webAppUrl }}],
-          // [{text: 'Make a Test Payment', callback_data: 'pay'}]
         ],
         resize_keyboard: true,
       }
-    })
+    });
   });
 
-  // // Handling button clicks
-  // bot.on('callback_query', async (query) => {
-  //   if (query.data === 'pay') {
-  //     await bot.sendInvoice(
-  //       query.from.id,
-  //       "Test Product",
-  //       "Example description",
-  //       "payload",
-  //       process.env.PROVIDER_TOKEN,
-  //       "USD",
-  //       [{"label": "Test Product", "amount": 1000}], // amount in the smallest currency unit
-  //     );
-  //   }
-  // });
-  //
-  // // Handling pre-checkout query
-  // bot.on('pre_checkout_query', (query) => {
-  //   bot.answerPreCheckoutQuery(query.id, true).catch(console.error);
-  // });
-  //
-  // // Handling successful payment
-  // bot.on('successful_payment', (message) => {
-  //   bot.sendMessage(message.chat.id, "Thank you for your payment!");
-  // });
+  // Handling pre-checkout query
+  bot.on('pre_checkout_query', (preCheckoutQuery) => {
+    console.log("PreCheckoutQuery:", preCheckoutQuery);
+    bot.answerPreCheckoutQuery(preCheckoutQuery.id, true)
+      .then((result) => {
+        console.log("PreCheckoutQuery answered:", result);
+      })
+      .catch((error) => {
+        console.error("Error in answering PreCheckoutQuery:", error);
+      });
+  });
+
+  // Handling successful payment
+  bot.on('successful_payment', (message) => {
+    bot.sendMessage(message.chat.id, "Thank you for your payment!");
+  });
 
   console.log('Bot is running...')
 };
